60ab36c5f718568684571ac1899bf839abb86efb
BelladonnaEraes <alnison@mail.ru>
2022-06-27 21:23:04 +0300

    index.html

--- Week2/Day2/DailyChallenge/script.js
+++ Week2/Day2/DailyChallenge/script.js
@@ -7,10 +7,22 @@ let sentence = "The movie is not that bad, I like it";
 /*Create a variable called wordNot where it’s value is the first appearance 
 (ie. the position) of the substring “not” (from the sentence variable).*/
 
-let wordNot = "The movie is bad, I like it"
+let wordNot = "not";
 
 /*Create a variable called wordBad where it’s value is the first appearance (
 ie. the position) of the substring “bad” (from the sentence variable).*/
 
+let wordBad = "bad";
 
-let wordBad = "The movie is not bad, I like it"
+/*If the word “bad” comes after the word “not”, you should replace the whole “not…bad” substring with “good”, then console.log the result.
+For example, the result here should be : “The movie is good, I like it”
+If the word “bad” does not come after “not” or the words are not in the sentence, console.log the original 
+sentence.*/
+
+if (sentence.search("bad")>sentence.search("not") {
+	console.log("This movie is bad!")
+} else { 
+sentence.search("bad") > sentence.search("not") {
+	console.log("This movie is good!")
+} 
+
