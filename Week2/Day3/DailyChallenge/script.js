bdcfe00bcea061faf978aa321ce06d9673c3f13b
BelladonnaEraes <alnison@mail.ru>
2022-06-28 21:20:01 +0300

    script.js

---
+++ Week2/Day3/DailyChallenge/script.js
@@ -0,0 +1,9 @@
+let star = ["*","**","***","****","*****","******"]
+for (let i=0; i<star.length; i++) {
+    console.log(star[i]);}
+
+
+let star1 = [" ", "*","**","***","****","*****","******"]
+for (let i=0; i<1; i++)	
+for (let j=0; j<star1.length; j++) {
+	console.log(star1[i]+star1[j]);}
