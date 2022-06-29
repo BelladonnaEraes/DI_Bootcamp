996ce754d401dd9a657f766ebf3a47ae068e1e9f
Amichai Berger <76897731+Amichaiz@users.noreply.github.com>
2022-06-29 17:06:09 +0300

    Update script.js

--- Week2/Day3/DailyChallenge/script.js
+++ Week2/Day3/DailyChallenge/script.js
@@ -7,3 +7,17 @@ let star1 = [" ", "*","**","***","****","*****","******"]
 for (let i=0; i<1; i++)	
 for (let j=0; j<star1.length; j++) {
 	console.log(star1[i]+star1[j]);}
+
+next time try using built in javascript methods like repeat 
+
+another salution would be 
+let twoPyramid = '';
+
+for (i = 0; i < 6; i++) {
+	for (j = 0; j <= i; j++) {
+        twoPyramid += '*';
+    }
+    twoPyramid += '\n';
+}
+
+console.log(twoPyramid)
