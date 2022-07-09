d6790be9ea9813ea84e6f7c2d2d1959e7364c6a2
BelladonnaEraes <alnison@mail.ru>
2022-07-09 21:05:33 +0300

    r

---
+++ Week3/Day3/ExercisesXP/script.js
@@ -0,0 +1,64 @@
+/*In your Javascript file, using setTimeout, call a function after 2 seconds.
+The function will alert “Hello World”.*/
+
+    //function playGame() {
+    //setTimeout(alert("Hello,world!"), 2000)}
+    //playGame()
+
+/*In your Javascript file, using setTimeout, call a function after 2 seconds.
+The function will add a new paragraph <p>Hello World</p> to the <div id="container">.*/
+
+
+//function playGame() {
+//     setTimeout(helloWorld, 2000)}
+//playGame()
+//function helloWorld() {
+//   let container = document.getElementById("container")
+//    let text = document.createTextNode("Hello World");
+//    container.appendChild(text) 
+//    }
+/*In your Javascript file, using setInterval, call a function every 2 seconds.
+The function will add a new paragraph <p>Hello World</p> to the <div id="container">.
+The interval will be cleared (ie. clearInterval), when the user will click on the button.
+Instead of clicking on the button, the interval will be cleared (ie. clearInterval) as 
+soon as there will be 5 paragraphs inside the <div id="container">.*/
+
+/*let clear = document.getElementById("clear")
+let timer = setTimeout(helloWorld, 2000)
+let container = document.getElementById("container")
+let text = document.createTextNode("Hello World");
+
+function helloWorld() {  
+    container.appendChild(text) 
+    }
+clear.addEventListener("click", clearText)
+
+function clearText () {
+    clearInterval(timer)
+}*/
+/*let clear = document.getElementById("clear")
+let timer=setInterval(playGame, 2000)
+let counter = 0 
+function playGame() { 
+  
+    if (counter == 5) 
+    { clearInterval(timer) 
+    } else {
+  
+    let container = document.getElementById("container")
+    let text = document.createTextNode("Hello World");
+    container.appendChild(text) 
+    counter++  
+    }
+ }/*
+/*-----------------------------------------------------------*/
+
+/*Copy the code above, to a structured HTML file.
+In your Javascript file, use setInterval to move the <div id="animate"> to the right side of the <div id="container">, when the button is clicked on.
+The <div id="animate"> should move 1px to the right every milisecond, until it reaches the end of the <div id="container">.
+Hint : use clearInterval as soon as the box reaches the right end side of the container
+Hint : check out the demonstration in the Course Noted named JS Functions*/
+
+
+
+
