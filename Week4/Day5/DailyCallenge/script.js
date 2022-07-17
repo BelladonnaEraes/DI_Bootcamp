9fe0681587e4434d871375cb30a8cbffc36a283b
BelladonnaEraes <alnison@mail.ru>
2022-07-17 09:38:51 +0300

    111

--- Week4/Day5/DailyCallenge/script.js
+++ Week4/Day5/DailyCallenge/script.js
@@ -2,7 +2,7 @@
 function comparison (arr1, arr2) {
 	let arr11 = Array.from(arr1).sort()	
 	let arr22 = Array.from(arr2).sort()
-	console.log(arr11.toString() === arr22.toString())
+	return console.log(arr11.toString() === arr22.toString())
 }
 
 let Astr = "astronomer"
@@ -13,5 +13,5 @@ let code = "themorsecode"
 let dots = "herecomedots"
 
 comparison(Moon, Astr)
-comparison(scoll, room)
-comparison(code, dots)
+//comparison(scoll, room)
+//comparison(code, dots)
