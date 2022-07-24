62a6e0a88d37a71da3154c47a71dd62f1d04f8b1
BelladonnaEraes <alnison@mail.ru>
2022-07-24 19:18:10 +0300

    24.07.2022

---
+++ Week6/Day1/ExercisesXP/Week6Day1ExersiceXP.sql
@@ -0,0 +1,28 @@
+--CREATE TABLE item(
+--item_id SERIAL PRIMARY KEY,
+--item_name VARCHAR (50) NOT NULL,
+-- item_quantity SMALLINT NOT NULL
+--);
+--INSERT INTO item(item_name, item_quantity)
+--VALUES ('Small Desk', 100),
+--       ('Large desk', 300),
+--       ('Fan', 80);
+--CREATE TABLE customers(
+--customers_id SERIAL PRIMARY KEY,
+--customers_first_name VARCHAR (50) NOT NULL,
+--customers_last_name VARCHAR (50) NOT NULL
+--);
+--INSERT INTO customers(customers_first_name, customers_last_name)
+--VALUES ('Greg', 'Jones'),
+--       ('Sandra', 'Jones'),
+--	   ('Scott', 'Scott'),
+--	   ('Trevor', 'Green'),
+--      ('Melanie', 'Johnson');
+
+--SELECT * FROM item
+--SELECT item_name FROM item WHERE item_quantity>80
+--SELECT item_name FROM item WHERE item_quantity>=300
+--SELECT * FROM customers WHERE customers_last_name='Smith'
+--SELECT * FROM customers WHERE customers_last_name='Jones'
+--SELECT * FROM customers WHERE customers_first_name !='Scott'
+
