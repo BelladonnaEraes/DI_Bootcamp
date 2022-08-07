66e343b0f2fe59d1b568e3784ed0185c6be0f6f3
BelladonnaEraes <alnison@mail.ru>
2022-08-07 19:37:56 +0300

    7.08.22

---
+++ Week8/Day1/ExercisesXP/homework/src/Exercise4.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+const Exercise4 =() => {
+  return (
+    <div>
+     <h1>This is a Header<h1>
+     <p>This is paragraph</p>
+     <a 
+     href="https://reactjs.org"
+     target="_blank"
+     rel="noopener noreferrer">This ia a link</a>
+     <h3>This is a Form:</h3>
+     <lable>Enter your name</lable>
+     <imput>
+     <h6>Here is an Image:</h6>
+      <img src={logo}  alt="logo" />
+     </div>
+   )
+}
+
+export default Exercise4
