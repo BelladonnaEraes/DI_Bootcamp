b5366d98a473c892238d29e68e45e077fe79bb7d
BelladonnaEraes <alnison@mail.ru>
2022-08-08 11:11:09 +0300

    8.08.22

--- Week8/Day1/ExercisesXP/homework/src/Exercise4.js
+++ Week8/Day1/ExercisesXP/homework/src/Exercise4.js
@@ -1,19 +1,21 @@
 import logo from './logo.svg';
+import './exercise.css';
+
 const Exercise4 =() => {
   return (
-    <div>
-     <h1>This is a Header<h1>
-     <p>This is paragraph</p>
-     <a 
+  <div>  <div>
+     <h1>This is a Header</h1>
+     <p className='para'>This is paragraph</p>
+     <a
      href="https://reactjs.org"
      target="_blank"
      rel="noopener noreferrer">This ia a link</a>
      <h3>This is a Form:</h3>
      <lable>Enter your name</lable>
-     <imput>
+     <imput/>
      <h6>Here is an Image:</h6>
       <img src={logo}  alt="logo" />
-     </div>
+     </div></div>
    )
 }
 
---
+++ Week8/Day3/counter/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './component/Counter.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       < Counter name ={'John'}/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
