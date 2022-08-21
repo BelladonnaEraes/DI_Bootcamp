f23271137154aff0559566fdb2246672b36e332f
BelladonnaEraes <alnison@mail.ru>
2022-08-21 09:28:44 +0300

    21.08.22

--- Week9/Day5/DailyCallenge/movieseries/src/components/SearchBut.js
+++ Week9/Day5/DailyCallenge/movieseries/src/components/SearchBut.js
@@ -4,14 +4,15 @@ import {connect} from 'react-redux'
 const SearchBut = (props)=>{
      const handleClick = () => {
     const searchkey = props
-    console.log(searchkey)
+    //console.log(searchkey)
+    fetch(`http://www.omdbapi.com/?apikey=14600d9e&t=${searchkey}&plot=full`)
+        .then(response => response.json())
+        .then(response=> {
+          console.log(response)
+        })
+        .catch(err=> console.log(err))
   }
-fetch(`http://www.omdbapi.com/?apikey=14600d9e&t=${searchkey}&plot=full`)
-    .then(response => response.json())
-    .then(response=> {
-      console.log(response)
-    })
-    .catch(err=> console.log(err))
+
   return (
     <div>
     <div><button onClick={handleClick}>Search</button></div>
