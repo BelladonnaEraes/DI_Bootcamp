57b7848a2771144cec561996af29f51e64209ab3
BelladonnaEraes <alnison@mail.ru>
2022-09-05 20:00:19 +0300

    5.09.22

--- Week11/customers-project 2/server/routes/Users.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-import {Register,Login} from '../controllers/Users.js';
-
-router.post('/signup', Register);
-router.post('/signin', Login);
-
-export default router;
