3194d822e8bb9137c19f4e1a402032802a7923ef
BelladonnaEraes <alnison@mail.ru>
2022-09-11 17:42:56 +0300

    make it better

--- finalProject/server/server.js
+++ finalProject/server/server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import readylist_router from './routes/Readylist.js'
 import cookieParser from 'cookie-parser';
 import autorization_router from './routes/Autorization.js'
-
+import path from 'path'
 dotenv.config();
 
 const app = express();
@@ -12,10 +12,17 @@ const app = express();
 app.listen(process.env.PORT||8080, ()=>{
   console.log(`run on port ${process.env.PORT||8080}`);
 })
-app.use(express.static(__dirname + '/public'));
+
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
 app.use(autorization_router);
 app.use(readylist_router);
+
+
+const __dirname = path.resolve()
+app.use('/', express.static(path.join(__dirname, 'client/build')))
+app.get('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'./client/build', 'index.html'))
+})
